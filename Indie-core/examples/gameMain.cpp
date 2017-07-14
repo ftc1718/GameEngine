6b9ae878073804e75d169dd83aba61bade1fa3b6
1091939302 <1091939302@qq.com>
2017-07-14 09:07:34 +0800

    add web example

--- Indie-core/examples/gameMain.cpp
+++ Indie-core/examples/gameMain.cpp
@@ -6,18 +6,3 @@ int main()
 	game.start();
 	return 0;
 }
-
-//#include <iostream>
-//#include <functional>
-//
-//void print_hello() { std::cout << "hello" << std::endl; }
-//
-//int main()
-//{
-//	std::function<void()> func = [&]()
-//	{ std::cout << "hello" << std::endl; };
-//	std::function<void()> f = func;
-//	f();
-//	std::cin.get();
-//	return 0;
-//}
